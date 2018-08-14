e9ee458cb59ca071ecc156d9cb16e88fdf5bbb67
张鑫 <913726265@qq.com>
2018-08-14 16:35:44 +0800

    修复首页、商品详情页、发布商品

--- js/module/index.js
+++ js/module/index.js
@@ -47,17 +47,18 @@ function goodsHtml(goods) {
     var html =
         "<li title='" + goods.goodsName + "'>" +
         "<a href='/goodsDetails.html?id=" + goods.id + "'>" +
-        "<img style='height: 270px;margin-bottom: 0px;' src=\"" + IMAGE_URL + goods.goodsMasterImgs.split(",")[0] + "\" class=\"home_list_goods\"/>" +
+        "<div class='home_list_goods'>" +
+        "<img src='" + IMAGE_URL + goods.goodsMasterImgs.split(',')[0] + "'/></div>" +
         "<h3>" + goods.goodsName + "</h3>" +
         "<h4>" + goods.goodsDesc + "</h4>" +
         "<div class=\"home_list_price\">" +
         "<h5 class=\"fl\"><span style=\"font-size: 16px;\">￥</span>&nbsp;" + goods.shopPrice + "&nbsp;" +
         "<span style=\"font-size: 14px;color: #E85526;\">元</span></h5>" +
         "<img src=\"img/sign_free.png\" class=\"fr\"/>" +
-        "<h6>" + isFreeShipping
-        + "</h6></div>" +
+        "<h6>" + isFreeShipping +
+        "</h6></div>" +
         "<h6 class=\"home_list_time\">剩余：<span id='timer" + goods.id + "'></span></h6>" +
-        "</a></li>"
+        "</a></li>";
     return html;
 }
 
